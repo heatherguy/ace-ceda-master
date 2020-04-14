bd6144413af8d0288702d4b46868c535c79a0e2a
Heather Guy <guyh@jasmin-sci5.ceda.ac.uk>
2020-04-14 13:23:25 +0100

    updated netcdf generator

--- parse-scripts/parse_flux-estimates.py
+++ parse-scripts/parse_flux-estimates.py
@@ -139,8 +139,8 @@ def main():
         
         # Get licor data
     
-        if os.path.isfile(in_loc+'LiCOR/licor_%s'%day_str):
-            licor = pd.read_csv(in_loc+'LiCOR/licor_%s'%day_str, index_col=0, parse_dates=[0])
+        if os.path.isfile(in_loc+'licor/licor_%s'%day_str):
+            licor = pd.read_csv(in_loc+'licor/licor_%s'%day_str, index_col=0, parse_dates=[0])
         else:
             print('Error: Licor File empty, '+day_str)
             continue
