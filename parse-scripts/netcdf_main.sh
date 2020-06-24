92f834e0d97d219f620a6586d473df50894b5847
Heather Guy <guyh@jasmin-sci5.ceda.ac.uk>
2020-06-24 15:12:46 +0100

    jasmin aerosol updates

--- parse-scripts/netcdf_main.sh
+++ parse-scripts/netcdf_main.sh
@@ -10,8 +10,8 @@ conda activate guyh
 # Generate netcdf file
 meta_dir='/gws/nopw/j04/ncas_radar_vol1/heather/ace-ceda-master'
 netcdf_out='/gws/nopw/j04/ncas_radar_vol1/heather/final_nc/'
-months=[6,7,8,9,10,11,12]
-years=[2019]
+months=[1,2,3,4]
+years=[2020]
 avp=1
 
 
@@ -24,13 +24,13 @@ in_loc_proc='/gws/nopw/j04/ncas_radar_vol1/heather/processed/'
 #python parse_surface-moisture-profile.py $in_loc $netcdf_out $months $years $avp
 #python parse_surface-winds-profile.py $in_loc $netcdf_out $months $years $avp
 #python parse_aerosol-concentration.py $in_loc_proc $netcdf_out $months $years $avp
-#python parse_aerosol-size-distribution.py $in_loc_proc $netcdf_out $months $years $avp
+python parse_aerosol-size-distribution.py $in_loc_proc $netcdf_out $months $years $avp
 
 start_dat='201906010000'
 stop_dat='202001010000'
 
-python parse_flux-estimates.py $in_loc_proc $netcdf_out $start_dat $stop_dat 30 1
-python parse_flux-estimates.py $in_loc_proc $netcdf_out $start_dat $stop_dat 30 2
-python parse_flux-estimates.py $in_loc_proc $netcdf_out $start_dat $stop_dat 15 1
-python parse_flux-estimates.py $in_loc_proc $netcdf_out $start_dat $stop_dat 15 2
+#python parse_flux-estimates.py $in_loc_proc $netcdf_out $start_dat $stop_dat 30 1
+#python parse_flux-estimates.py $in_loc_proc $netcdf_out $start_dat $stop_dat 30 2
+#python parse_flux-estimates.py $in_loc_proc $netcdf_out $start_dat $stop_dat 15 1
+#python parse_flux-estimates.py $in_loc_proc $netcdf_out $start_dat $stop_dat 15 2
 
