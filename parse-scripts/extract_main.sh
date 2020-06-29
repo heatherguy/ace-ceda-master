f27338281fff5a0f2cd5570290d512f19622ac7d
Heather Guy <guyh@jasmin-sci5.ceda.ac.uk>
2020-06-29 09:55:39 +0100

    jasmin update

--- parse-scripts/extract_main.sh
+++ parse-scripts/extract_main.sh
@@ -8,35 +8,38 @@ source /home/users/guyh/miniconda3/etc/profile.d/conda.sh
 conda activate guyh
 
 # Extract data files from .raw
-ext_start='200501'
+ext_start='190101'
 ext_stop='200601'
 raw_dir_flux='/gws/nopw/j04/ncas_radar_vol2/data/ICECAPSarchive/fluxtower/raw/'
 raw_dir_ace='/gws/nopw/j04/ncas_radar_vol2/data/ICECAPSarchive/ace/raw/'
 extract_dir='/gws/nopw/j04/ncas_radar_vol1/heather/extracted/'
 
 
-echo 'Extracting SnD'
-python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'SnD'
+#echo 'Extracting SnD'
+#python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'SnD'
 
-echo 'Extracting HMP'
-python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'HMP'
+#echo 'Extracting HMP'
+#python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'HMP'
 
-echo 'Extracting KT'
-python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'KT'
+#echo 'Extracting KT'
+#python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'KT'
 
-echo 'Extracting CPC'
-python tar_extract.py $ext_start $ext_stop $raw_dir_ace $extract_dir 'CPC'
+#echo 'Extracting CPC'
+#python tar_extract.py $ext_start $ext_stop $raw_dir_ace $extract_dir 'CPC'
 
-echo 'Extracting SKYOPC'
-python tar_extract.py $ext_start $ext_stop $raw_dir_ace $extract_dir 'SKYOPC'
+#echo 'Extracting SKYOPC'
+#python tar_extract.py $ext_start $ext_stop $raw_dir_ace $extract_dir 'SKYOPC'
 
-echo 'Extracting licor'
-python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'licor'
+echo 'Extracting msf-opc'
+python tar_extract.py $ext_start $ext_stop $raw_dir_ace $extract_dir 'Summit_MSF'
 
-echo 'Extracting metek'
-python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'metek'
+#echo 'Extracting licor'
+#python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'licor'
 
-echo 'Extracting ventus'
-python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'ventus'
+#echo 'Extracting metek'
+#python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'metek'
+
+#echo 'Extracting ventus'
+#python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'ventus'
 
 
