84a321a9fd55c90bdfe3e48db1c2ecfeaf58c777
Heather Guy <guyh@host595.jc.rl.ac.uk>
2021-03-24 10:28:57 +0000

    update to aerosol qc and process

--- parse-scripts/extract_main.sh
+++ parse-scripts/extract_main.sh
@@ -11,8 +11,8 @@ source /home/users/guyh/miniconda3/etc/profile.d/conda.sh
 conda activate guyh
 
 # Extract data files from .raw
-ext_start='201015'
-ext_stop='210301'
+ext_start='200701'
+ext_stop='200901'
 raw_dir_flux='/gws/nopw/j04/ncas_radar_vol2/data/ICECAPSarchive/fluxtower/raw/'
 raw_dir_ace='/gws/nopw/j04/ncas_radar_vol2/data/ICECAPSarchive/ace/raw/'
 extract_dir='/gws/nopw/j04/ncas_radar_vol1/heather/extracted/'
@@ -30,19 +30,19 @@ extract_dir='/gws/nopw/j04/ncas_radar_vol1/heather/extracted/'
 #echo 'Extracting CPC'
 #python tar_extract.py $ext_start $ext_stop $raw_dir_ace $extract_dir 'CPC'
 
-#echo 'Extracting SKYOPC'
-#python tar_extract.py $ext_start $ext_stop $raw_dir_ace $extract_dir 'SKYOPC'
+echo 'Extracting SKYOPC'
+python tar_extract.py $ext_start $ext_stop $raw_dir_ace $extract_dir 'SKYOPC'
 
-#echo 'Extracting msf-opc'
-#python tar_extract.py $ext_start $ext_stop $raw_dir_ace $extract_dir 'Summit_MSF'
+echo 'Extracting msf-opc'
+python tar_extract.py $ext_start $ext_stop $raw_dir_ace $extract_dir 'Summit_MSF'
 
 #echo 'Extracting licor'
 #python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'licor'
 
-echo 'Extracting metek'
-python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'metek'
+#echo 'Extracting metek'
+#python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'metek'
 
-echo 'Extracting ventus'
-python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'ventus'
+#echo 'Extracting ventus'
+#python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'ventus'
 
 
