82e3b5ad1def750cdbad0de690ad15dbaa01d8f0
Heather Guy <guyh@host596.jc.rl.ac.uk>
2023-03-16 13:46:28 +0000

    2022 jan to june process

--- parse-scripts/process_main.sh
+++ parse-scripts/process_main.sh
@@ -27,22 +27,22 @@ tawo_opc_dir='/gws/nopw/j04/ncas_radar_vol1/heather/extracted/tawo-opc/'
 
 
 
-all_start='20220804000'
-all_stop='202302150000'
+all_start='20220101000'
+all_stop='20220601000'
 hmp_path='/gws/nopw/j04/ncas_radar_vol1/heather/processed/HMP/'
 kt_qcf='/gws/nopw/j04/ncas_radar_vol1/heather/ace-ceda-master/qc-files/KT_bad_dates'
 skyopc_qc='/gws/nopw/j04/ncas_radar_vol1/heather/ace-ceda-master/qc-files/skyopc_percent_loss_data.csv'
 
-#python process_raw.py $hmp_dir $proc_dir 'HMP' $all_start $all_stop
-#python process_raw.py $snd_dir $proc_dir 'SnD' $all_start $all_stop $hmp_path
-#python process_raw.py $kt_dir $proc_dir 'KT' $all_start $all_stop $kt_qcf
-#python process_raw.py $v_dir $proc_dir 'ventus' $all_start $all_stop
-#python process_raw.py $m_dir $proc_dir 'metek' $all_start $all_stop
-#python process_raw.py $li_dir $proc_dir 'licor' $all_start $all_stop
+python process_raw.py $hmp_dir $proc_dir 'HMP' $all_start $all_stop
+python process_raw.py $snd_dir $proc_dir 'SnD' $all_start $all_stop $hmp_path
+python process_raw.py $kt_dir $proc_dir 'KT' $all_start $all_stop $kt_qcf
+python process_raw.py $v_dir $proc_dir 'ventus' $all_start $all_stop
+python process_raw.py $m_dir $proc_dir 'metek' $all_start $all_stop
+python process_raw.py $li_dir $proc_dir 'licor' $all_start $all_stop
 #python process_raw.py $cpc_dir $proc_dir 'CPC' $all_start $all_stop
 #python process_raw.py $sky_dir $proc_dir 'SKYOPC' $all_start $all_stop $skyopc_qc
 #python process_raw.py $opc_dir $proc_dir 'MSF_OPC' $all_start $all_stop
-python process_raw.py $tawo_opc_dir $proc_dir 'TAWO_OPC' $all_start $all_stop
+#python process_raw.py $tawo_opc_dir $proc_dir 'TAWO_OPC' $all_start $all_stop
 
 
 
