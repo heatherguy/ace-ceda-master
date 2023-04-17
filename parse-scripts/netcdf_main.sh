9e24cca316c73cf3430c05479d2446ce49cf3605
Heather Guy <guyh@host425.jc.rl.ac.uk>
2023-04-17 16:45:27 +0100

    include present weather

--- parse-scripts/netcdf_main.sh
+++ parse-scripts/netcdf_main.sh
@@ -16,8 +16,8 @@ conda activate guyh
 # Generate netcdf file
 meta_dir='/gws/nopw/j04/ncas_radar_vol1/heather/ace-ceda-master'
 netcdf_out='/gws/nopw/j04/ncas_radar_vol1/heather/final_nc/'
-months=[11]
-years=[2021]
+months=[8,9,10,11,12]
+years=[2022]
 avp=1
 
 
@@ -33,12 +33,13 @@ in_loc_proc='/gws/nopw/j04/ncas_radar_vol1/heather/processed/'
 #python parse_aerosol-size-distribution.py $in_loc_proc $netcdf_out $months $years $avp
 #python parse_aerosol-opc.py $in_loc_proc $netcdf_out $months $years $avp
 #python parse_aerosol-opc_TAWO.py $in_loc_proc $netcdf_out $months $years $avp
+python parse_present-weather.py $in_loc_proc $netcdf_out $months $years
 
 start_dat='202112170000'
 stop_dat='202201010000'
 
 #python parse_flux-estimates.py $in_loc_proc $netcdf_out $start_dat $stop_dat 30 1
-python parse_flux-estimates.py $in_loc_proc $netcdf_out $start_dat $stop_dat 30 2
+#python parse_flux-estimates.py $in_loc_proc $netcdf_out $start_dat $stop_dat 30 2
 #python parse_flux-estimates.py $in_loc_proc $netcdf_out $start_dat $stop_dat 15 1
 #python parse_flux-estimates.py $in_loc_proc $netcdf_out $start_dat $stop_dat 15 2
 
