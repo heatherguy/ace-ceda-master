c381bf8618c6dd0c192bde10939db49f787ed828
Heather Guy <heatherguy@users.noreply.github.com>
2023-09-29 14:07:06 +0100

    Remove duplicates from licor data

--- parse-scripts/parse_flux-estimates.py
+++ parse-scripts/parse_flux-estimates.py
@@ -149,6 +149,7 @@ def main():
         if os.path.isfile(in_loc+'licor/licor_%s'%day_str):
             licor = pd.read_csv(in_loc+'licor/licor_%s'%day_str, index_col=0, parse_dates=[0])
             licor[licor['QC']!=1]=np.nan
+            licor = licor[~licor.index.duplicated()]
         else:
             print('Error: Licor File empty, '+day_str)
             continue
