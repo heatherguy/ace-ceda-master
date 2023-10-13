cb10d863f9e2d89224d88786beb43b200474f028
Heather Guy <guyh@host596.jc.rl.ac.uk>
2023-10-13 13:06:07 +0100

    updated present weather and skyopc search

--- parse-scripts/extract_main.sh
+++ parse-scripts/extract_main.sh
@@ -12,7 +12,7 @@ conda activate guyh
 
 # Extract data files from .raw
 ext_start='220801'
-ext_stop='220915'
+ext_stop='221001'
 
 raw_dir_flux='/gws/nopw/j04/icecaps/ICECAPSarchive/fluxtower/raw/'
 #raw_dir_flux='/gws/nopw/j04/ncas_radar_vol1/heather/fluxtower_temp/'
@@ -24,17 +24,20 @@ extract_dir='/gws/nopw/j04/ncas_radar_vol1/heather/extracted/'
 #echo 'Extracting SnD'
 #python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'SnD'
 
+#echo 'Extracting biral'
+#python tar_extract.py $ext_start $ext_stop $raw_dir_ace $extract_dir 'biral'
+
 #echo 'Extracting HMP'
 #python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'HMP'
 
-echo 'Extracting KT'
-python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'KT'
+#echo 'Extracting KT'
+#python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'KT'
 
 #echo 'Extracting CPC'
 #python tar_extract.py $ext_start $ext_stop $raw_dir_ace $extract_dir 'CPC'
 
-#echo 'Extracting SKYOPC'
-#python tar_extract.py $ext_start $ext_stop $raw_dir_ace $extract_dir 'SKYOPC'
+echo 'Extracting SKYOPC'
+python tar_extract.py $ext_start $ext_stop $raw_dir_ace $extract_dir 'skyopc'
 
 #echo 'Extracting msf-opc'
 #python tar_extract.py $ext_start $ext_stop $raw_dir_ace $extract_dir 'MSF'
