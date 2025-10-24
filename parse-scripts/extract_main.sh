d359e72a9596e958270fff37624c1be717816b48
heatherguy <guyh@host839.jc.rl.ac.uk>
2025-10-24 16:55:44 +0100

    updated sbatch commands

--- parse-scripts/extract_main.sh
+++ parse-scripts/extract_main.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-#SBATCH --partition=short-serial
+#SBATCH --job-name="ICECAPS-extract_main"
+#SBATCH --partition=standard
+#SBATCH --qos=standard
+#SBATCH --account=icecaps
 #SBATCH -o /gws/nopw/j04/ncas_radar_vol1/heather/logs/%j.out 
 #SBATCH -e /gws/nopw/j04/ncas_radar_vol1/heather/logs/%j.err
-#SBATCH -t 24:00:00
+#SBATCH --time 24:00:00
+
+
+
 # Check we're in the right directory
 cd /gws/nopw/j04/ncas_radar_vol1/heather/ace-ceda-master/parse-scripts
 
@@ -11,8 +17,8 @@ source /home/users/guyh/miniconda3/etc/profile.d/conda.sh
 conda activate guyh
 
 # Extract data files from .raw
-ext_start='220801'
-ext_stop='221001'
+ext_start='230701'
+ext_stop='250101'
 
 raw_dir_flux='/gws/nopw/j04/icecaps/ICECAPSarchive/fluxtower/raw/'
 #raw_dir_flux='/gws/nopw/j04/ncas_radar_vol1/heather/fluxtower_temp/'
@@ -39,11 +45,14 @@ extract_dir='/gws/nopw/j04/ncas_radar_vol1/heather/extracted/'
 echo 'Extracting SKYOPC'
 python tar_extract.py $ext_start $ext_stop $raw_dir_ace $extract_dir 'skyopc'
 
+echo 'Extracting POPS'
+python tar_extract.py $ext_start $ext_stop $raw_dir_ace $extract_dir 'F2023'
+
 #echo 'Extracting msf-opc'
 #python tar_extract.py $ext_start $ext_stop $raw_dir_ace $extract_dir 'MSF'
 
-#echo 'Extracting tawo-opc'
-#python tar_extract.py $ext_start $ext_stop $raw_dir_ace $extract_dir 'TAWO'
+echo 'Extracting tawo-opc'
+python tar_extract.py $ext_start $ext_stop $raw_dir_ace $extract_dir 'TAWO'
 
 #echo 'Extracting licor'
 #python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'licor'
@@ -54,5 +63,5 @@ python tar_extract.py $ext_start $ext_stop $raw_dir_ace $extract_dir 'skyopc'
 #echo 'Extracting ventus'
 #python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'ventus'
 
-echo 'Extracting simba'
-python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'simba'
+#echo 'Extracting simba'
+#python tar_extract.py $ext_start $ext_stop $raw_dir_flux $extract_dir 'simba'
